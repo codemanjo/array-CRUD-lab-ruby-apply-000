16353968465ad0138debce09a4d728a5cdefa23e
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-18 22:53:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,6 +19,7 @@ end
 add_element_to_start_of_array(array,element)
 
 def remove_element_from_end_of_array(array)
+  element =["coleslaw"]
   array.pop("coleslaw")
 end
 
@@ -27,7 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
